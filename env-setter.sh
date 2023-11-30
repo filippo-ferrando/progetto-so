ba84e943397c135adb075f56aa6332a9d1dff65f
Filippo Ferrando Damillano <ferrandodfilippo@protonmail.com>
2023-11-30 12:54:53 +0100

    setter for env variable

---
+++ env-setter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Set environment variables
+
+echo Setting necessary environment variables...
+
+export ENERGY_DEMAND=100
+echo Setting ENERGY_DEMAND to $ENERGY_DEMAND
+
+export N_ATOMI_INIT=100
+echo Setting N_ATOMI_INIT to $N_ATOMI_INIT
+
+export N_ATOM_MAX=20
+echo Setting N_ATOM_MAX to $N_ATOM_MAX
+
+export MIN_N_ATOMICO=1
+echo Setting MIN_N_ATOMICO to $MIN_N_ATOMICO
+
+export N_NUOVI_ATOMI=5
+echo Setting N_NUOVI_ATOMI to $N_NUOVI_ATOMI
+
+export SIM_DURATION=1000
+echo Setting SIM_DURATION to $SIM_DURATION
+
+export ENERGY_EXPLODE_THRESHOLD=1000
+echo Setting ENERGY_EXPLODE_THRESHOLD to $ENERGY_EXPLODE_THRESHOLD
