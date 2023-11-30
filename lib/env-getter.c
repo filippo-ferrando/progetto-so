a17370b0ee76386e12239907cffadcced059736c
Filippo Ferrando Damillano <ferrandodfilippo@protonmail.com>
2023-11-30 13:07:25 +0100

    lib for get env variable

---
+++ lib/env-getter.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    printf("Env variable getter\n");
+
+    int ENERGY_DEMAND = getenv("ENERGY_DEMAND");
+    int N_ATOMI_INIT = getenv("N_ATOMI_INIT");
+    int N_ATOM_MAX = getenv("N_ATOM_MAX");
+    int MIN_N_ATOMICO = getenv("MIN_N_ATOMICO");
+    int N_NUOVI_ATOMI = getenv("N_NUOVI_ATOMI");
+    int SIM_DURATION = getenv("SIM_DURATION");
+    int ENERGY_EXPLODE_THRESHOLD = getenv("ENERGY_EXPLODE_THRESHOLD");
+
+}
