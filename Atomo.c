a2d400379716133e42d16e3d34438e8b001c9e6e
Filippo Ferrando Damillano <ferrandodfilippo@protonmail.com>
2023-12-06 19:27:52 +0100

    completed atomo function

--- Atomo.c
+++ Atomo.c
@@ -6,6 +6,7 @@ int main(int argc, char* argv[]){
     int sem_sm = semget(KEY_SEM_SM, 9, 0666); //Semaforo per accesso a variabili da stampare
     int sem_att = semget(KEY_ATT, 1, 0666); //Semaforo per accesso a variabili da stampare
     int n_atomico = atoi(argv[1]);
+    int min_n_atomico = atoi(argv[3]);
 
     //printf("invocato\n");
     struct stats *st;
@@ -13,44 +14,90 @@ int main(int argc, char* argv[]){
     st = shmat(shmid, NULL, 0);
 
     if(atoi(argv[2]) != -1){
-        if(reserveSem(sem_start, 0) < 0){
-            perror("reserveSem");
-            exit(1);
-        }
+        reserveSem(sem_start, 0);
     }
 
     while(1){
-        //sblocca semaforo
-        //forka
-        //printf("atomo %d\n", getpid());
-        //sleep(1);
-        //assegna numero atomico
-        //controlla scrap
-        //aggiorna stats
-
-        if(reserveSem(sem_sm, 0) < 0){
-            perror("reserveSem attivazione atomo: ");
+        //################# TEST #################
+        /*
+        if(atoi(argv[2]) == -1){
+            printf("atomo creato da alimentatore %d\n", getpid());
+        }else{
+            printf("atomo creato da master %d\n", getpid());
+        }
+        */
+        //################# TEST #################
+        
+        if(reserveSem(sem_att, 0) < 0){
+            perror("reserveSem attivatore atomo: ");
             exit(1);
         }
+        //printf("qua\n");
+
+        //calcolo numero atomico
+        n_atomico = n_atomico / 2;
+
+        //controllo numero atomico e aumento scrap
+        if(n_atomico <= min_n_atomico){
+            if(reserveSem(sem_sm, 8) < 0){
+                perror("reserveSem sm scrap atomo: ");
+                exit(1);
+            }
+            st->scrap++;
+            //printf("atomo %d scrap\n", getpid());
+            if(releaseSem(sem_sm, 8) < 0){
+                perror("releaseSem sm scrap atomo: ");
+                exit(1);
+            }
+            exit(0);
+        }
+
+        //forko
         switch(fork()){
             case -1:
                 perror("fork atomo: ");
                 //segnale di meltdown
-                exit(1);
+                exit(6);
             case 0:
-                //calcolo numero atomico
-                //entro sezione critica di semaforo attivazione
-                //entro in sezione critica di sm
-                //scrivo energia rilasciata
-                //esco sezione critica di sm
                 //controllo se n_atomico < MIN_N_ATOMICO -> se si cancello atomo -> scrap++
-                //forko
-                //esco sezione critica di attivazione
-            default:
-                //calcolo n_atomico padre
-        }
+                //printf("atomo figlio %d creato\n", getpid());
+                if(n_atomico <= min_n_atomico){
+                    if(reserveSem(sem_sm, 8) < 0){
+                        perror("reserveSem sm scrap atomo: ");
+                        exit(1);
+                    }
+                    //printf("atomo figlio %d scrap\n", getpid());
+                    st->scrap++;
+                    if(releaseSem(sem_sm, 8) < 0){
+                        perror("releaseSem sm scrap atomo: ");
+                        exit(1);
+                    }
+                    exit(0);
+                }
 
-        exit(0);
+                //entro in sezione critica di sm per 
+                if(reserveSem(sem_sm, 2) < 0){
+                    perror("reserveSem sm atomo: ");
+                    exit(1);
+                }
+                if(reserveSem(sem_sm, 4) < 0){
+                    perror("reserveSem sm atomo: ");
+                    exit(1);
+                }
+                //printf("atomo figlio %d in senzione critica\n", getpid());
+                st->split_ls++;
+                st->energy_created_ls = st->energy_created_ls + 7;
+
+                //esco sezione critica di sm
+                if(releaseSem(sem_sm, 2) < 0){
+                    perror("releaseSem sm atomo: ");
+                    exit(1);
+                }
+                if(releaseSem(sem_sm, 4) < 0){
+                    perror("releaseSem sm atomo: ");
+                    exit(1);
+                }
+        }
     }
     //printf("SEM VAL atomo: %d\n", semctl(semid, 0, GETVAL));
 
