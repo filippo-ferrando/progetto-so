1e8d8f6e97f7aed471e8bcd4ec8bc795510738b1
Filippo Ferrando Damillano <ferrandodfilippo@protonmail.com>
2023-12-07 12:21:26 +0100

    debugging

--- Attivatore.c
+++ Attivatore.c
@@ -9,25 +9,35 @@ struct message_buff {
 };
 
 int main(){
-    int sem_start = semget(KEY_SEM_ACT, 1, 0666);
-    int sem_sm = semget(KEY_SEM_SM, 9, 0666);
+    int sem_start = semget(KEY_SEM_ACT, 1, IPC_CREAT | 0666);
+    int sem_sm = semget(KEY_SEM_SM, 9, IPC_CREAT | 0666);
     int sem_att = semget(KEY_ATT, 1, IPC_CREAT | 0666);
+
     int n_attivazioni = 10;
+
     int msgid = msgget(KEY_INHIB,IPC_CREAT|0600); //msgid tiene id per comunicare con inibitore
     struct message_buff messaggio; //Tiene il messaggio che riceve dall'inibitore
-    messaggio.mtype = 1; //mtype a 1 per comunicare con l'inibitore 
+    messaggio.mtype = 1; //mtype a 1 per comunicare con l'inibitore
+
+    FILE *ipcs_id = fopen("ipcs_id_q.txt", "a");
+    fprintf(ipcs_id, "%d\n", msgid);
+    fclose(ipcs_id);
+
     struct stats *st;
     int shmid = shmget(KEY_SHM, sizeof(st), 0666);
     st = shmat(shmid, NULL, 0);
     
     reserveSem(sem_start, 0);
+    printf("SONO ATTIVATORE | HO PID %d\n", getpid());
 
     while(1){
         msgrcv(msgid,&messaggio,sizeof(messaggio) - sizeof(long),1,0); //Dopo che leggo un messaggio, svuoto coda di messaggi.
         if(messaggio.mex == 0){  //Se leggo 1 dall'inibitore procedo, altrimenti sto aspetto di ricevere quel valore dalla receive
+            printf("attivatore riceve 0\n");
             sleep(1);
             //svuota coda di messaggi
         }else if(messaggio.mex == 1){
+            printf("attivatore riceve 1\n");
             sleep(5);
             //svuota coda di messaggi
         }
@@ -37,16 +47,19 @@ int main(){
             exit(1);
         }
         printf("attivatore in sezione critica sm\n");
-        st->activations_ls+=10;
+        st->activations_ls+=n_attivazioni;
         if(releaseSem(sem_sm, 0) < 0){
             perror("releaseSem sm attivazioni attivatore: ");
             exit(1);
         }
         printf("attivatore rilasica sem att\n");
-        semctl(sem_att, 0, SETVAL, n_attivazioni);
+        if(semctl(sem_att, 0, SETVAL, n_attivazioni) < 0){
+            perror("semctl attivatore: ");
+            exit(1);
+        }
         
     }
     //printf("SEM VAL attivatore: %d\n", semctl(semid, 0, GETVAL));
-    printf("SONO ATTIVATORE | HO PID %d\n", getpid());
+    
     exit(0);
 }
