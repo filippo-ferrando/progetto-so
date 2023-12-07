767330e48545cee021b8e8c50cb61175a16879b6
Filippo Ferrando Damillano <ferrandodfilippo@protonmail.com>
2023-12-07 12:22:57 +0100

    ipc remove system

--- lib/killer.sh
+++ lib/killer.sh
@@ -10,5 +10,25 @@ killall -9 Attivatore.out > /dev/null 2>&1
 killall -9 Inibitore.out > /dev/null 2>&1
 killall -9 Master.out > /dev/null 2>&1
 
+# close ipc resources
+filename="ipcs_id_sem.txt"
+filename1="ipcs_id_mem.txt"
+filename2="ipcs_id_q.txt"
+
+for sem in $(cat $filename)
+do
+    ipcrm -s $sem > /dev/null 2>&1
+done
+
+for mem in $(cat $filename1)
+do
+    ipcrm -m $mem > /dev/null 2>&1
+done
+
+for q in $(cat $filename2)
+do
+    ipcrm -q $q > /dev/null 2>&1
+done
+
 echo $'Done.\n'
 exit 0
