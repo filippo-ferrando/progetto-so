d1e2a63aa4503345da016b3cb531f98a7dd4b739
Filippo Ferrando Damillano <ferrandodfilippo@protonmail.com>
2023-12-11 21:09:11 +0100

    Update environment variables

--- env-setter.sh
+++ env-setter.sh
@@ -4,25 +4,25 @@
 
 echo Setting necessary environment variables...
 
-export ENERGY_DEMAND=10
+export ENERGY_DEMAND=500
 echo Setting ENERGY_DEMAND to $ENERGY_DEMAND
 
-export N_ATOMI_INIT=50
+export N_ATOMI_INIT=15
 echo Setting N_ATOMI_INIT to $N_ATOMI_INIT
 
-export N_ATOM_MAX=200
+export N_ATOM_MAX=118
 echo Setting N_ATOM_MAX to $N_ATOM_MAX
 
 export MIN_N_ATOMICO=1
 echo Setting MIN_N_ATOMICO to $MIN_N_ATOMICO
 
-export N_NUOVI_ATOMI=25
+export N_NUOVI_ATOMI=10
 echo Setting N_NUOVI_ATOMI to $N_NUOVI_ATOMI
 
 export SIM_DURATION=60
 echo Setting SIM_DURATION to $SIM_DURATION
 
-export ENERGY_EXPLODE_THRESHOLD=10000
+export ENERGY_EXPLODE_THRESHOLD=1000000
 echo Setting ENERGY_EXPLODE_THRESHOLD to $ENERGY_EXPLODE_THRESHOLD
 
 export STEP=599999999
